0e78f2ef9d7254e64fc4b01b4da92bacb9363528
Sally Moore <sallyamoore@gmail.com>
2015-06-11 14:28:03 -0700

    Changed logic in initialize to create a new db only if one didn't already exist.

--- lib/database.rb
+++ lib/database.rb
@@ -6,17 +6,22 @@ module TaskList
 
     def initialize(database_name)
       @database_name = "db/#{ database_name }"
-      db = SQLite3::Database.new @database_name
-      db.execute "CREATE TABLE tasklist (id INTEGER PRIMARY KEY, Name TEXT NOT NULL, Description TEXT, Date_completed TEXT);"
+      try_to_create_db
     end
 
+    def try_to_create_db
+      unless File.exists?(@database_name)
+        db = SQLite3::Database.new(@database_name)
+        db.execute "CREATE TABLE tasklist (id INTEGER PRIMARY KEY, Name TEXT NOT NULL, Description TEXT, Date_completed TEXT);"
+      end
+    end
 
     private
 
     def query!(statement, *params)
       db = SQLite3::Database.open(database_name)
       db.execute statement #, params
-    # rescue SQLite3::Exception => error
+     rescue SQLite3::Exception => error
       # use this block to recover from an error
       # consider giving the user a special message back
       # inspect the `error` object for information about the error
